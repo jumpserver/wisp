ff7ecb7e76a4d7e5cd2d00d469e8d8e31d1abce7
Eric <xplzv@126.com>
2022-02-16 18:52:22 +0800

    perf: 获取token的连接信息

--- pkg/jms-sdk-go/service/jms_token.go
+++ pkg/jms-sdk-go/service/jms_token.go
@@ -11,3 +11,25 @@ func (s *JMService) GetTokenAsset(token string) (tokenUser model.TokenUser, err 
 	_, err = s.authClient.Get(Url, &tokenUser)
 	return
 }
+
+func (s *JMService) GetConnectTokenAuth(token string) (resp TokenAuthInfo, err error) {
+	data := map[string]string{
+		"token": token,
+	}
+	_, err = s.authClient.Post(TokenAuthInfoURL, data, &resp)
+	return
+}
+
+type TokenAuthInfo struct {
+	Id          string             `json:"id"`
+	Secret      string             `json:"secret"`
+	TypeName    string             `json:"type"`
+	User        model.User         `json:"user"`
+	Actions     []string           `json:"actions,omitempty"`
+	Application *model.Application `json:"application,omitempty"`
+	Asset       *model.Asset       `json:"asset,omitempty"`
+	ExpiredAt   int64              `json:"expired_at"`
+	Gateway     *model.Gateway     `json:"gateway,omitempty"`
+
+	SystemUserAuthInfo model.SystemUserAuthInfo `json:"system_user"`
+}
